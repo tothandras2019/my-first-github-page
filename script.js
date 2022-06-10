d7251ebf610add56a933617f2511ad70f3fd5937
Andras <toth.andras.2000@gmail.com>
2022-06-10 11:37:12 +0200

    Modified and add new button pressed feature!

---
+++ script.js
@@ -0,0 +1,11 @@
+/** @format */
+
+`use strict`;
+
+const button = document.querySelector(`.btn--message`);
+
+button.addEventListener(`click`, (e) => {
+    e.preventDefault();
+    button.textContent = `Thanks for review! 👍`;
+    button.style.fontSize = `30px`;
+});
